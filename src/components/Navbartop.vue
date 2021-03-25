ca621fb0abd7316ae7887775fd9bd229da5432bf
rodrigosantosdev <rodrigofrontend@gmail.com>
2021-03-25 00:25:45 -0300

    add navbartop

---
+++ src/components/Navbartop.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="navbartop">
+   <a href="#" class="new-order-btn">+ New Order</a>
+   <div class="profile-sign-in">
+    <img src="https://i.pravatar.cc/150?img=3" alt="">
+    <h4>Hi John Doe</h4>
+    <i class='bx bxs-down-arrow'></i>
+   </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .navbartop {
+    width: 100%;
+    height: 80px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+    padding: 0 30px;
+    background-color: var(--dark);
+  }
+
+  .new-order-btn {
+    color: var(--primary);
+    font-size: 24px;
+  }
+</style>
---
+++ src/components/NewOrder.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1 class="title">Order List</h1>
+    <section class="new-order">
+        <p>Ola voce chegou na dashboard Ueba!!! Parabens</p>    
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .new-order {
+        height: 100%;
+        margin-top: 30px;
+        margin-left: 30px;
+        margin-right: 30px;
+        border-top: 4px solid var(--primary);
+        background-color: var(--white);
+    }
+
+    .title {
+        margin-left: 30px;
+        margin-top: 30px;
+    }
+</style>
