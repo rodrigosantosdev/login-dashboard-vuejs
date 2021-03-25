6d0240f973e1e7fd2a10eadd2af1d13d36aeca46
rodrigosantosdev <rodrigofrontend@gmail.com>
2021-03-25 00:25:09 -0300

    add main content component

---
+++ src/components/MainContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-content">
+    <Navbartop />
+    <NewOrder />
+  </div>
+</template>
+
+<script>
+import Navbartop from './Navbartop.vue'
+import NewOrder from './NewOrder.vue'
+
+export default {
+  components: { Navbartop, NewOrder },
+}
+
+</script>
+
+<style>
+ .main-content {
+   width: 100%;
+   height: 100%;
+
+   display: flex;
+   flex-direction: column;
+   background-color:var(--secondary);
+  }
+</style>
