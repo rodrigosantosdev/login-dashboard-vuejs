dc39cd37bce99bd33dd9cd30544910c19af7ebbb
rodrigosantosdev <rodrigofrontend@gmail.com>
2021-03-28 21:38:28 -0300

    create profile component

---
+++ src/components/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="profile-sign-in">
+    <img src="https://picsum.photos/200" alt="">
+    <h4>Hi, John Doe</h4>
+    <i class='bx bxs-down-arrow'></i>
+   </div>
+</template>
+
+<style lang="scss">
+
+.profile-sign-in {
+   display: flex;
+   align-items:center;
+   border-left: 2px solid var(--border);
+   padding-left: 25px;
+   height:80px;
+
+   img {
+   width: 35px;
+   height: 35px;
+   border-radius: 50%;
+   margin-right: 10px;
+   }
+
+   h4 {
+      color: var(--white);
+      font-weight: normal;
+   }
+
+   i {
+      color: var(--gray-light);
+      margin-left: 10px;
+   }
+}
+
+</style>
