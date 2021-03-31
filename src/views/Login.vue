9209974e39ad0b0543208841fdc3a1a099430130
rodrigosantosdev <rodrigofrontend@gmail.com>
2021-03-31 00:24:12 -0300

    ajuste de pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <Nav />
-    <ContentDescription />
+    <FormBanner />
     <FormLogin />
   </div>
 </template>
@@ -9,21 +9,53 @@
 <script>
 
 import Nav from '../components/Nav.vue'
-import ContentDescription from '../components/ContentDescription.vue'
+import FormBanner from '../components/FormBanner.vue'
 import FormLogin from '../components/FormLogin.vue'
 
 export default {
-  components: { Nav, ContentDescription, FormLogin },
+  components: { Nav, FormBanner, FormLogin },
 }
 
 </script>
 
 <style lang="scss">
+
 .content {
-  width: 1230px;
+  max-width: 1300px;
   height: 580px;
   display: flex;
   border-radius: 15px;
   background: var(--secondary);
+}
+
+@media ( max-width: 1260px ) {
+
+  .content {
+    width: 600px;
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+@media ( max-width: 700px ) {
+
+  .content {
+    width: 340px;
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+  }
+}
+
+@media ( max-width: 470px ) {
+
+  .content {
+    width: 320px;
+    margin: 0 auto;
+  }
 }
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,29 +1,60 @@
 <template>
   <div class="content">
-    <FormSignUp />
-    <ContentDescription />
     <Nav />
+    <FormBanner />
+    <FormSignUp />
   </div>
 </template>
 
 <script>
 
 import Nav from '../components/Nav.vue'
-import ContentDescription from '../components/ContentDescription.vue'
+import FormBanner from '../components/FormBanner.vue'
 import FormSignUp from '../components/FormSignUp.vue'
 
 export default {
-  components: { Nav, ContentDescription, FormSignUp },
+  components: { Nav, FormBanner, FormSignUp },
 }
 
 </script>
 
 <style lang="scss">
+
 .content {
-  width: 1230px;
+  max-width: 1300px;
   height: 580px;
   display: flex;
   border-radius: 15px;
   background: var(--secondary);
+}
+
+@media ( max-width: 1260px ) {
+
+  .content {
+    width: 600px;
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+@media ( max-width: 700px ) {
+
+  .content {
+    width: 340px;
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+@media ( max-width: 470px ) {
+
+  .content {
+    width: 320px;
+    margin: 0 auto;
+  }
 }
 </style>
